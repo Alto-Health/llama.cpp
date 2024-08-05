9b2931a5f75b29e4a8393ded19df777794cf01e8
Xiao Zhang <58770171+xiao11lam@users.noreply.github.com>
2024-08-05 12:37:43 +0100

    update

--- examples/server/server.cpp
+++ examples/server/server.cpp
@@ -986,7 +986,7 @@ struct server_context {
 
 					slot.originalPrompt = slot.prompt.get<std::string>();
 
-                	std::string add_prompt = " \n Based on the above information, do the name entity recogniton for me and return in the JSON format. The entityName is the name of the entity, and the entityDescript is the description of the entity. Return the JSON format with entityName, entityDescript, entityValue, where the entityValue is the value of the recogniton.";
+                	std::string add_prompt = " Here, The entity_name is the name of the entity, and the entity_description is the description of the entity. Now, based on the list here, Return the JSON format with entity_name, entity_description, entity_value, where the entity_value is the value of the entity, which is the value you need to extract from the name entities.";
 
 					std::string slot_prompt = slot.prompt.get<std::string>();
                 	slot_prompt += add_prompt;
